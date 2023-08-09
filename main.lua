435eecadfeedf0d83daf2d83b9ec8b09833056b5
liconaj <liconaj215@gmail.com>
2023-08-09 08:55:01 -0500

    Changed background and score appearance

--- main.lua
+++ main.lua
@@ -4,17 +4,19 @@ debug=false
 gravity=0.15
 boost=2.5
 
+bgstr="`ト`ト`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ナ`ろag`u`れaa`faa`u`るaa`gaa`u`kao`せaa`haa`u`kaa`maa`せaa`haa`u`kaa`maa`すaa`iaa`nab`e`kah`faa`しaa`jaa`mad`d`raa`faa`さaa`kaa`maa`caa`c`raa`faa`こaa`laa`lab`cab`bah`iab`faa`けaa`maa`laa`eaa`b`gab`gab`gaa`くaa`naa`jac`eab`a`hab`dac`haa`きaa`oaa`iab`haa`a`iaf`jaa`きaa`oaa`iaa`iab`yaa`かaa`paj`kaa`yaa`おaa`♥`yaa`あad`☉`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`あaa`⬅️`yaa`iax`yaa`⬅️`yaa`iaa`vaa`yaa`wal`h`yaa`iaa`vaa`yaa`waa`jaa`h`yaa`iaa`vaa`yaa`waa`jaa`h`yaa`iaa`ian`yaa`waa`jaa`h`yaa`iaa`iaa`~ai`waa`jaa`h`yaa`iaa`iaa`|ac`○aa`jaa`h`yaa`iaa`iaa`|aa`▒aa`jaa`h`yaa`iaa`iaa`|aa`▒aa`jaa`h`yaa`iaa`iab`{aa`▒aa`jaa`h`yaa`iaa`jab`pac`gaa`▒aa`jai`yaa`iaa`kab`oaa`aaa`fab`oas`s`yaa`iaa`lab`naa`aaa`faa`paa`✽`yak`mab`maa`aaa`eab`paa`✽ar`█aa`maa`aaa`dab`qaa`✽`qaa`▒ab`kaa`aaa`cab`raa`✽`qaa`🐱ae`gaa`aaa`bab`sak`{`qaa`♥aa`faa`aaa`baa`~aa`{`qaa`♥ab`eaa`aaa`aab`~aa`{`qaa`☉ag`aac`○aa`{`qaa`のaa`{`qaa`のaa`{`qaa`のaa`{`qaa`のaa`{`qaa`のaa`{`qaa`のaa`{`qaa`のaa`{`qaa`のaa`{`qap`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`こaa`{`yah`こaa`{`yaa`ちaa`{`yaa`ちaa`{`yaa`ちaa`{`yah`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`こaa`{`█aa`◆al`haa`{`{af`◆aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`{aa`⬆️aa`jaa`haa`{`zab`⬆️aa`jaa`haa`{`yaa`∧aa`jaa`haa`{`xab`∧aa`jaa`haa`{`wab`❎aa`jaa`haa`{`waa`▤aa`jaa`haa`{`uab`qa🐱`faa`jaa`haa`{`tab`raa`█aa`faa`jaa`haa`{`taa`saa`○ab`faa`jaa`haa`{`taj`jaa`~ab`gaa`jaa`haa`{`}aa`jaa`|ac`haa`jaa`haa`{`~ab`haa`|aa`jaa`jaa`haa`{`tah`baa`iaa`|aa`jaa`jaa`haa`{`taa`gac`iaa`|aa`jaa`jaa`haa`{`taa`haa`jaa`|aa`jaa`jaa`haa`{`taa`saa`|ab`iaa`jaa`haa`{`tab`rab`|ak`jaj`{`taa`taa`へ`taa`tab`ふ`taa`uaa`ふ`tai`maa`ふ`|ak`caa`ふ`●ab`bab`ひ`♥ab`baa`ひ`☉aa`bab`は`☉ab`aab`は`⬅️ab`は`⌂ab`ひ`ナ`ナ`ナ`ナ`ナ`ナ"
+
 function _init()
     t=0
 	game={}    
 	btnrel=true
 	chsaws=nil
     make_parts()
-	make_waves()
-	make_background()
+	make_waves()	
 	restart()	
 	lsaws=make_saws(1,-1,7,false)	
 	rsaws=make_saws(1,121,113,true)	
+	bgtbl=s2t(bgstr)
 end
 
 function _update60()	
@@ -62,19 +64,16 @@ end
 function _draw()
 	cls(bgcol)	
 	pal(6,0)
-	--pal(9,8)
-	draw_background()
+	rle1(bgstr,0,0)
 	draw_saws(lsaws)
 	draw_saws(rsaws)
 	draw_waves()
 	map(0,0,0,0,16,16)	
-	if not game.lose and game.started then
+	if not game.lose then
 		draw_player()		
 	end
-	if not game.started then
-		print("press x to start",33,70,7)
-	else
-		cprint(score,32,5)
+	if game.started then
+		cprint(score,75,13,true)
 	end
 	draw_pads()
     draw_parts()
@@ -102,10 +101,14 @@ function zfill(txt,n)
 	return "\*"..(n-#txt).."0"..txt
 end
 
-function cprint(txt,y,c)
+function cprint(txt,y,c,big)	
 	txt=tostr(txt)
-	local x=63-(#txt*6-2)/2	
-	print("\^w\^t"..txt,x,y,c)
+	local x=big and 63-(#txt*6-2)/2	or 63-2*#txt
+	if big then
+		print("\^w\^t"..txt,x,y,c)
+	else
+		print(txt,x,y,c)
+	end
 end
 
 function rndr(l,r)
@@ -131,20 +134,31 @@ function draw_coll(o,col)
 	rectfill(o.x,o.y,o.x+o.w,o.y+o.h,col)
 end
 
-function make_background()
-	background={}
-	for _=1,5 do
-		add(background,{
-			x=rndr(20,100),
-			y=rndr(20,100),
-			r=rndr(20,80),
-			col=1
-		})
-	end
+
+function rle1(s,x0,y,tr)
+ local x,mw=x0,x0+ord(s,2)-96
+ for i=5,#s,2do
+  local col,len=ord(s,i)-96,ord(s,i+1)-96
+  if(col!=tr) line(x,y,x+len-1,y,col)
+  x+=len if(x>mw) x=x0 y+=1
+ end
 end
 
-function draw_background()
-	for obj in all(background) do
-		circ(obj.x,obj.y,obj.r,obj.col)
-	end
+function s2t(s)
+ local t={}
+ for i=2,#s,2 do
+  add(t,ord(s,i-1)*256+ord(s,i)-24672)
+ end
+ return t
+end
+
+function rle2(t,x0,y0,tr)
+ local x,y,mw=x0,y0,x0+t[1]
+  for i=3,#t do
+   local v=t[i]
+   local col,len=v\256,v&255
+   if(col!=tr) line(x,y,x+len-1,y,col)
+   x+=len
+  if(x>mw) x=x0 y+=1
+ end
 end
